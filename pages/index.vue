e3d91f8c685e306f06247e3477cde487ef1e2478
Olakunle Boye <kunlexzy@gmail.com>
2019-10-10 12:09:25 +0100

    form added

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,65 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-cont
-      </h1>
-      <h2 class="subtitle">
-        Concatenate nuxt work testing
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div class="main">
+    <div class="wrapper">
+      <form @submit.pevent="handleSubmit">
+        <div class="form-group">
+          <input type="text" class="form-control" v-model="form.name" placeholder="Full name">
+        </div>
+        <div class="form-group">
+          <input type="email" class="form-control" v-model="form.email" placeholder="Email address">
+        </div>
+        <div class="form-group">
+          <input type="password" class="form-control" placeholder="Password">
+        </div>
+        <button type="submit" class="btn">Create</button>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
-  components: {
-    Logo
+  data(){
+    return{
+      form:{}
+    }
+  },
+  methods: {
+    async handleSubmit(){
+      console.log('Hurray it works')
+    }
   }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+.main {
+  height: 100vh;
+  width: 100%;
   display: flex;
-  justify-content: center;
   align-items: center;
-  text-align: center;
 }
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+.wrapper {
+  width: 400px;
+  margin: 0 auto;
+  background: #ccc;
+  padding: 15px;
+}
+.form-group {
+  padding: 0 0 10px 0;
+}
+.form-control {
+  width: 100%;
+  height: 40px;
+  color: #000;
+}
+.btn {
+  width: 100%;
+  height: 40px;
   display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  background: #0333cc;
+  border: #0333cc solid 1px;
+  color: #fff;
 }
 </style>
